a2bebdc845b3f7bb744a44ae18ceacc1a13563e5
jayguygao <gaoxiandathss@gmail.com>
2015-07-15 13:09:48 +0800

    no message

--- homework/hw4/picture_wall.js
+++ homework/hw4/picture_wall.js
@@ -207,7 +207,7 @@ function getWindowHeight(){
 
 //判断滚动条是否到底部
 window.onscroll = function (evt) {
-	if(getScrollTop() + getWindowHeight() == getScrollHeight()){
+	if(getScrollTop() + getWindowHeight() +50 >= getScrollHeight()){
 		alert("you are in the bottom!");
 		if (imageInfo != null){
 			addPicture(10);
