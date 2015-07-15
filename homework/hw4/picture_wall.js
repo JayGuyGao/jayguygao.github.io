6021ea9e5d1f410693668a49c95a34e6be0d1f87
jayguygao <gaoxiandathss@gmail.com>
2015-07-15 13:03:59 +0800

    no message

--- homework/hw4/picture_wall.js
+++ homework/hw4/picture_wall.js
@@ -167,11 +167,48 @@ function clickbg(evt) {
 }
 //document.body.addEventListener("click", , false);
 
+
+//滚动条在Y轴上的滚动距离
+function getScrollTop(){
+　　var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+　　if(document.body){
+　　　　bodyScrollTop = document.body.scrollTop;
+　　}
+　　if(document.documentElement){
+　　　　documentScrollTop = document.documentElement.scrollTop;
+　　}
+　　scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+　　return scrollTop;
+}
+
+//文档的总高度
+function getScrollHeight(){
+　　var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+　　if(document.body){
+　　　　bodyScrollHeight = document.body.scrollHeight;
+　　}
+　　if(document.documentElement){
+　　　　documentScrollHeight = document.documentElement.scrollHeight;
+　　}
+　　scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+　　return scrollHeight;
+}
+
+//浏览器视口的高度
+function getWindowHeight(){
+　　var windowHeight = 0;
+　　if(document.compatMode == "CSS1Compat"){
+　　　　windowHeight = document.documentElement.clientHeight;
+　　}else{
+　　　　windowHeight = document.body.clientHeight;
+　　}
+　　return windowHeight;
+}
+
+//判断滚动条是否到底部
 window.onscroll = function (evt) {
-	var scrollTop = document.body.scrollTop;
-	var offsetHeight = document.body.offsetHeight;
-	var clientHeight = document.body.clientHeight;
-	if (scrollTop >= offsetHeight - clientHeight) {
+	if(getScrollTop() + getWindowHeight() == getScrollHeight()){
+		alert("you are in the bottom!");
 		if (imageInfo != null){
 			addPicture(10);
 		}
